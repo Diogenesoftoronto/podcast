58d0cd4f93abcf114d527cf166c19dc74c3eb063
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-06-05 18:30:38 -0400

    format

--- podcasts/doubleZeroes.js
+++ podcasts/doubleZeroes.js
@@ -1,21 +1,22 @@
 // this function doubles the zeroes in a array. the array stays the same length
 
-function doubleZeroes (arr) {
-  let result = arr; let split;
+function doubleZeroes(arr) {
+  let result = arr;
+  let split;
 
   for (let i = 0; i < result.length; i++) {
     if (result[i] === 0) {
       result.pop();
-      split = result.splice(i+1);
+      split = result.splice(i + 1);
       result.push(0);
-      result = result.concat([...split]);
+      result = result.concat(split);
       i += 2;
     }
   }
   return result;
-};
+}
 
-let array = [1, 0, 2, 0, 3, 5, 0];
+let array = [1, 0, 2, 3, 0, 4, 5, 0];
 // console.log(typeof array)
-val = doubleZeroes(array)
+val = doubleZeroes(array);
 console.log(val);
